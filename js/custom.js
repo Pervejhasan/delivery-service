caf4be80d6b4e00c14b63c1f373ba38f6d7c55a3
Pervejhasan <mdpervej12441@gmail.com>
2023-02-10 01:24:24 +0600

    footer portion design complete

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+let section = document.querySelector(".container");
+let stat = document.querySelector(".stats .sta");
+let start = false;
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop) {
+    if (!start) {
+      stat.foreach((sta) => startCount(sta));
+    }
+    start = true;
+  }
+};
+
+function startCount(el) {
+  let max = el.dataset.max;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == max) {
+      clearInterval(count);
+    }
+  }, 2000 / stat);
+}
